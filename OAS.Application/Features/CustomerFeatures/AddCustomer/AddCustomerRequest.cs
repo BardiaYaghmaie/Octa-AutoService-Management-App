edc2b7d72bc804bd9e11fa8b4d925ea2ff0d0ac6
MohammadYSF <aa.yosefiyan7@gmail.com>
2023-10-07 02:44:49 +0330

    add Customer feature started to code in backend

---
+++ OAS.Application/Features/CustomerFeatures/AddCustomer/AddCustomerRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using OAS.Application.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAS.Application.Features.CustomerFeatures.AddCustomer
+{
+    public sealed record AddCustomerRequest(string FirstName , string LastName , string phoneNumber,
+        DateTime RegisterDate , List<VehicleDTO> VehicleDTOs):IRequest<AddCustomerResponse>;   
+}
---
+++ OAS.Application/Features/CustomerFeatures/AddCustomer/AddCustomerResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAS.Application.Features.CustomerFeatures.AddCustomer
+{
+    public class AddCustomerResponse
+    {
+    }
+}

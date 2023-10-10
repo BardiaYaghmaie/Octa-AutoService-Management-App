f6323d63c46c4a79ec5b30284cd493a9ef674372
MohammadYSF <aa.yosefiyan7@gmail.com>
2023-10-11 01:59:50 +0330

    front:working on new inventory item

--- OAS.Persistence/Configs/InventoryItemHistoryConfig.cs
+++ OAS.Persistence/Configs/InventoryItemHistoryConfig.cs
@@ -15,9 +15,9 @@ public class InventoryItemHistoryConfig : IEntityTypeConfiguration<InventoryItem
         builder.Property(a => a.Name).HasMaxLength(255).IsRequired(true);
         builder.Property(a => a.Code).IsRequired(true);
         builder.Property(a => a.UpdateDate).IsRequired(true);
-        builder.Property(a => a.CountLowerBound).IsRequired(true);
-        builder.Property(a => a.BuyPrice).IsRequired(true);
-        builder.Property(a => a.SellPrice).IsRequired(true);        
+        builder.Property(a => a.CountLowerBound).IsRequired(false);
+        builder.Property(a => a.BuyPrice).IsRequired(false);
+        builder.Property(a => a.SellPrice).IsRequired(false);        
     }
 }
 

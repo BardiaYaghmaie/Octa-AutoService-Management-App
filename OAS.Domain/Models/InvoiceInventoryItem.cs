2d00146feebebadad9e48851fce8d50a3c516c33
Bardia Yaghmaie <bardia_yaghmaie@mathdep.iust.ac.ir>
2023-10-11 20:56:47 +0330

    nothing

--- OAS.Domain/Models/InvoiceInventoryItem.cs
+++ OAS.Domain/Models/InvoiceInventoryItem.cs
@@ -1,10 +1,13 @@
-﻿namespace OAS.Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OAS.Domain.Models;
 
 public class InvoiceInventoryItem
 {
     public Guid Id { get; set; }
     public Guid InvoiceId { get; set; }
     public Guid InventoryItemId { get; set; }
+    
     public virtual Invoice Invoice { get; set; }
     public virtual InventoryItem InventoryItem { get; set; }
 

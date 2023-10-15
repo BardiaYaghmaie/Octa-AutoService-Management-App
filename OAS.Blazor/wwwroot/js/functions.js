ca200f0a5c911c20a734c0be8f0bfed3a2a88031
Bardia Yaghmaie <bardia_yaghmaie@mathdep.iust.ac.ir>
2023-10-15 21:56:58 +0330

    iono

---
+++ OAS.Blazor/wwwroot/js/functions.js
@@ -0,0 +1,5 @@
+window.checkInternetConnection = function() {
+    // Implement your logic to check the internet connection status
+    // Return a boolean value based on the result
+    return navigator.onLine;
+}

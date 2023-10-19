83d87fbfe0fa81b6ab626ac1b8dc1bd8b619d656
MohammadYSF <aa.yosefiyan7@gmail.com>
2023-10-19 23:38:25 +0330

    usings refactoring and removing unnecessary code

--- OAS.Blazor/Program.cs
+++ OAS.Blazor/Program.cs
@@ -18,7 +18,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddMudServices();
 builder.Services.AddRadzenComponents();
 builder.Services.ConfigurePersistence(builder.Configuration);

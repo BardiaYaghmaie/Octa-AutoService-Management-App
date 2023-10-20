bc1044923dba29080dc1a6ee57f39bfa87ddb2d5
MohammadYSF <aa.yosefiyan7@gmail.com>
2023-10-20 18:34:11 +0330

    number formattings

--- OAS.Blazor/Utils/PersianCalenderUtils.cs
+++ OAS.Blazor/Utils/PersianCalenderUtils.cs
@@ -2,6 +2,35 @@
 
 namespace OAS.Blazor.Utils
 {
+    public static class PersianFormatting
+    {
+        public static string Fa2En(this string str)
+        {
+            return str.Replace("۰", "0")
+                      .Replace("۱", "1")
+                      .Replace("۲", "2")
+                      .Replace("۳", "3")
+                      .Replace("۴", "4")
+                      .Replace("۵", "5")
+                      .Replace("۶", "6")
+                      .Replace("۷", "7")
+                      .Replace("۸", "8")
+                      .Replace("۹", "9");
+        }
+        public static string En2Fa(this string str)
+        {
+            return str.Replace("0","۰")
+                      .Replace("1","۱")
+                      .Replace("2","۲")
+                      .Replace("3","۳")
+                      .Replace("4","۴")
+                      .Replace("5","۵")
+                      .Replace("6","۶")
+                      .Replace("7","۷")
+                      .Replace("8","۸")
+                      .Replace("9", "۹");
+        }
+    }
     public  static class PersianCalenderUtils
     {
         public static string ToPersianDateString(this DateTime dateTime)

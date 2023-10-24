e9d1dc66e0213995bea7e13d2216154bac6f412c
MohammadYSF <aa.yosefiyan7@gmail.com>
2023-10-24 10:27:34 +0330

    grid customer ok

---
+++ OAS.Application/Features/CustomerFeatures/GetCustomers/GetCustomersResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAS.Application.Features.CustomerFeatures.GetCustomers
+{
+    public record GetCustomersResponse
+    {
+        public int Count { get; set; }
+        public List<GetCustomersResponse_DTO> Data { get; set; }
+    }
+}
---
+++ OAS.Application/Features/CustomerFeatures/GetCustomers/GetCustomersResponse_DTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAS.Application.Features.CustomerFeatures.GetCustomers
+{
+    public record GetCustomersResponse_DTO
+    {
+        public int RowNumber{ get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Code { get; set; }
+    }
+}

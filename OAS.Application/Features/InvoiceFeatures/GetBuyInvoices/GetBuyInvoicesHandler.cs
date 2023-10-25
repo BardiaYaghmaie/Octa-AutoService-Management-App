af0108a8234d9ca60cca499aa492ded2a4fd5baa
MohammadYSF <aa.yosefiyan7@gmail.com>
2023-10-25 23:02:20 +0330

    some code

--- OAS.Application/Features/InvoiceFeatures/GetBuyInvoices/GetBuyInvoicesHandler.cs
+++ OAS.Application/Features/InvoiceFeatures/GetBuyInvoices/GetBuyInvoicesHandler.cs
@@ -13,33 +13,45 @@ namespace OAS.Application.Features.InvoiceFeatures.GetBuyInvoices
         private readonly IInvoiceRepository _invoiceRepository;
         private readonly IInventoryItemRepository _inventoryItemRepository;
         private readonly IInventoryItemHistoryRepository _inventoryItemHistoryRepository;
+        private readonly IServiceHistoryRepository _serviceHistoryRepository;
 
-        public GetBuyInvoicesHandler(IInvoiceRepository invoiceRepository, IInventoryItemRepository inventoryItemRepository, IInventoryItemHistoryRepository inventoryItemHistoryRepository)
+        public GetBuyInvoicesHandler(IInvoiceRepository invoiceRepository, IInventoryItemRepository inventoryItemRepository, IInventoryItemHistoryRepository inventoryItemHistoryRepository, IServiceHistoryRepository serviceHistoryRepository)
         {
             _invoiceRepository = invoiceRepository;
             _inventoryItemRepository = inventoryItemRepository;
             _inventoryItemHistoryRepository = inventoryItemHistoryRepository;
+            _serviceHistoryRepository = serviceHistoryRepository;
         }
+        private async Task<float> CalculateInvoiceTotalCost(Guid invoiceId)
+        {
+            float total = 0;
+            var invoice = await _invoiceRepository.GetById(invoiceId);
+            bool invoiceUseBuyPrice = invoice.UseBuyPrice.HasValue ? invoice.UseBuyPrice.Value : false;
+            var invoiceInventoryItems = await _invoiceRepository.GetInvoiceInventoryItemsByInvoiceId(invoiceId);
+            var invoiceServiceItems = await _invoiceRepository.GetInvoiceServicesByInvoiceIdAsync(invoiceId);
+            foreach (var item in invoiceInventoryItems)
+            {
+                var inventoryItemHistory = await _inventoryItemHistoryRepository.GetLatestByInventoryItemIdAndDateAsync(item.InventoryItemId, item.RegisterDate);
+                total += invoiceUseBuyPrice ? (item.Count * inventoryItemHistory.BuyPrice.Value) : (item.Count * inventoryItemHistory.SellPrice.Value);
+            }
 
+            foreach (var item in invoiceServiceItems)
+            {
+                var serviceHistory = await _serviceHistoryRepository.GetLatestServiceHistoryByServiceIdAndDate(item.ServiceId, item.RegisterDate);
+                total += (1 * item.Price);
+            }
+            return total;
+        }
         public async Task<GetBuyInvoicesResponse> Handle(GetBuyInvoicesRequest request, CancellationToken cancellationToken)
         {
             var invoices = await _invoiceRepository.GetAllAsync();
             var inventoryItemHistories = await _inventoryItemHistoryRepository.GetAllAsync();
             var invoicePayments = await _invoiceRepository.GetAllInvoicePaymentsAsync();
             var invoiceInventoryItems = await _invoiceRepository.GetAllInvoiceInventoryItemsAsync();
-            var data = invoices.Where(a => a.Type == Domain.Enums.InvoiceType.Buy).Select((a, i) =>
+            var data = invoices.Where(a => a.Type == Domain.Enums.InvoiceType.Buy).Select(async (a, i) =>
             {
                 long paidAmount = invoicePayments.Where(b => b.InvoiceId == a.Id).Select(b => b.PaidAmount).Sum();
-                float total = 0;
-                foreach (var invoiceInventoryItem in invoiceInventoryItems.Where(b => b.InvoiceId == a.Id).ToList())
-                {
-                    float count = invoiceInventoryItem.Count;
-                    DateTime registerDate = invoiceInventoryItem.RegisterDate;
-                    var inventoryItemId = invoiceInventoryItem.InventoryItemId;
-                    var buyPrice = inventoryItemHistories.Where(b => b.InventoryItemId == inventoryItemId && b.BuyPrice.HasValue && b.UpdateDate <= registerDate).OrderByDescending(b => b.UpdateDate).Select(b => b.BuyPrice.Value).First();
-                    total += (count * buyPrice);
-                }
-
+                float total = await this.CalculateInvoiceTotalCost(a.Id);
                 return new GetBuyInvoices_InvoiceDTO
                 (
                     SellerName: a.SerllerName,
@@ -53,7 +65,7 @@ namespace OAS.Application.Features.InvoiceFeatures.GetBuyInvoices
 
                 );
             }).ToList();
-            var response = new GetBuyInvoicesResponse(Data:data);
+            var response = new GetBuyInvoicesResponse(Data:(await Task.WhenAll(data)).ToList());
             return response;
             //var data = await _invoiceRepository.GetBuyInvoicesAsync();
             //return response;

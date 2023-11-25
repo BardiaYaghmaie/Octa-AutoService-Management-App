18b104c2dd43b3e0c201e7c7be857392e72cabe6
MohammadYSF <aa.yosefiyan7@gmail.com>
2023-11-26 00:46:41 +0330

    closes #21

--- OAS.Application/Features/InvoiceFeatures/GetDailySellInvoices/GetDailySellInvoicesResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿using OAS.Application.Features.InvoiceFeatures.GetSellInvoices;
-
-namespace OAS.Application.Features.InvoiceFeatures.GetDailySellInvoices
-{
-    public sealed record GetDailySellInvoicesResponse(List<GetDailySellInvoices_DTO> Data);
-}
